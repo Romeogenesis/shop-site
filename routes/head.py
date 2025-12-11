e6713a52ecfaefdeb3c222ea0540b3a24e3e7288
leonova-78@mail.ru <leonova-78@mail.ru>
2025-12-11 20:46:06 +0300

    comm1

--- routes/head.py
+++ routes/head.py
@@ -1,4 +1,4 @@
-from flask import request, Blueprint
+from flask import request, Blueprint, render_template # Добавлен render_template
 
 head_bp = Blueprint('head', __name__)
 
@@ -6,64 +6,19 @@ head_bp = Blueprint('head', __name__)
 @head_bp.route('/', methods=['GET', 'POST'])
 def registration():
     if request.method == 'GET':
-        html = '''
-        <!DOCTYPE html>
-        <html>
-        <head><title>Регистрация</title></head>
-        <body>
-          <h2>У вас уже есть аккаунт?</h2>
-          <form method="POST">
-            <label><input type="radio" name="has_account" value="yes" required> Да, хочу войти</label><br>
-            <label><input type="radio" name="has_account" value="no"> Нет, хочу создать новый</label><br><br>
-            <button type="submit">Далее</button>
-          </form>
-        </body>
-        </html>
-        '''
-        return html
+        # Вместо возврата строки HTML, рендерим шаблон index.html
+        return render_template('index.html')
 
     elif request.method == 'POST':
         has_account = request.form.get('has_account')
 
         if has_account == 'yes':
-            return '''
-            <!DOCTYPE html>
-            <html>
-            <body>
-              <h2>Вход</h2>
-              <form method="POST" action="/auth/submit">
-                <label>Логин: <input type="text" name="username" required minlength="3"></label><br><br>
-                <label>Пароль: <input type="password" name="password" required minlength="6"></label><br><br>
-
-                <button type="submit">Войти</button>
-              </form>
-              <a href="/">← Назад</a>
-            </body>
-            </html>
-            '''
+            # Рендерим шаблон login.html
+            return render_template('login.html')
 
         elif has_account == 'no':
-            html = '''
-            <!DOCTYPE html>
-            <html>
-            <head><title>Выбор роли</title></head>
-            <body>
-              <h2>Создание нового аккаунта</h2>
-              <form method="POST" action="/register/submit">
-                <label><input type="radio" name="role" value="user" required> Пользователь</label><br>
-                <label><input type="radio" name="role" value="keeper"> Кладовщик</label><br>
-                <label><input type="radio" name="role" value="admin"> Администратор</label><br><br>
-
-                <label>Логин: <input type="text" name="username" required minlength="3"></label><br><br>
-                <label>Пароль: <input type="password" name="password" required minlength="6"></label><br><br>
-
-                <button type="submit">Зарегистрироваться</button>
-              </form>
-              <a href="/">← Назад</a>
-            </body>
-            </html>
-            '''
-            return html
+            # Рендерим шаблон register.html
+            return render_template('register.html')
 
         else:
-            return "⚠️ Неверный выбор. <a href='/register'>Попробуйте снова</a>", 400
+            return "⚠️ Неверный выбор. <a href='/'>Попробуйте снова</a>", 400
